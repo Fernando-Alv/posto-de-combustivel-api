97219dadf719bb9d968572abdfabcce7434aac86
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-08-23 16:34:54 -0300

    feat: criar interface BombaDeCombustivelRepository

---
+++ src/main/java/com/meusprojetos/posto_combustivel/repository/BombaDeCombustivelRepository.java
@@ -0,0 +1,7 @@
+package com.meusprojetos.posto_combustivel.repository;
+
+import com.meusprojetos.posto_combustivel.entity.BombaDeCombustivel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BombaDeCombustivelRepository extends JpaRepository<BombaDeCombustivel, Long> {
+}
