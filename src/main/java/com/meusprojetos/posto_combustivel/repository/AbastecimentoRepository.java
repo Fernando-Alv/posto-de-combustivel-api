1456aabb7d5a7df58f83310640daad212d9eae2f
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-08-23 16:34:30 -0300

    feat: criar interface AbastecimentoRepository

---
+++ src/main/java/com/meusprojetos/posto_combustivel/repository/AbastecimentoRepository.java
@@ -0,0 +1,7 @@
+package com.meusprojetos.posto_combustivel.repository;
+
+import com.meusprojetos.posto_combustivel.entity.Abastecimento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AbastecimentoRepository extends JpaRepository<Abastecimento, Long> {
+}
