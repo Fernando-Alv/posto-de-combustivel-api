9cd4a05469fa82a5bbd54f5b6ec04e162be64701
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-08-25 13:36:30 -0300

    feat: criar TipoDeCombustivelService

---
+++ src/main/java/com/meusprojetos/posto_combustivel/service/TipoDeCombustivelService.java
@@ -0,0 +1,58 @@
+package com.meusprojetos.posto_combustivel.service;
+
+import com.meusprojetos.posto_combustivel.dto.TipoDeCombustivelDTO;
+import com.meusprojetos.posto_combustivel.entity.TipoDeCombustivel;
+import com.meusprojetos.posto_combustivel.mapper.TipoDeCombustivelMapper;
+import com.meusprojetos.posto_combustivel.repository.TipoDeCombustivelRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TipoDeCombustivelService {
+
+    private final TipoDeCombustivelRepository tipoDeCombustivelRepository;
+
+    private final TipoDeCombustivelMapper tipoDeCombustivelMapper;
+
+    public TipoDeCombustivelDTO create(TipoDeCombustivelDTO dto) {
+
+        TipoDeCombustivel entity = tipoDeCombustivelMapper.toEntity(dto);
+        TipoDeCombustivel salvo = tipoDeCombustivelRepository.save(entity);
+        return tipoDeCombustivelMapper.toDto(salvo);
+    }
+
+    public TipoDeCombustivelDTO findById(Long id) {
+
+        TipoDeCombustivel entity = getEntityById(id);
+        return tipoDeCombustivelMapper.toDto(entity);
+    }
+
+    public List<TipoDeCombustivelDTO> findAll() {
+
+        List<TipoDeCombustivel> entities = tipoDeCombustivelRepository.findAll();
+        return tipoDeCombustivelMapper.toDtoList(entities);
+    }
+
+    public TipoDeCombustivelDTO update(Long id, TipoDeCombustivelDTO dto) {
+        TipoDeCombustivel entity = getEntityById(id);
+
+        tipoDeCombustivelMapper.updateEntityFromDto(dto, entity);
+
+        TipoDeCombustivel atualizado = tipoDeCombustivelRepository.save(entity);
+        return tipoDeCombustivelMapper.toDto(atualizado);
+    }
+
+    public void delete(Long id) {
+        TipoDeCombustivel entity = getEntityById(id);
+
+        tipoDeCombustivelRepository.delete(entity);
+    }
+
+    private TipoDeCombustivel getEntityById(Long id) {
+        return tipoDeCombustivelRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Tipo de combustível não encontrado com o id: " + id));
+    }
+}
