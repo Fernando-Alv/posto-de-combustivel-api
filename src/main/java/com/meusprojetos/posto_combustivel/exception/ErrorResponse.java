7634d17745d42d544706451d9bd16a27f3d53437
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-08-28 14:28:57 -0300

    feat: criar ErrorResponse completo com timestamp, status, error, message e path

---
+++ src/main/java/com/meusprojetos/posto_combustivel/exception/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.meusprojetos.posto_combustivel.exception;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse (
+
+        LocalDateTime timestamp,
+        int status,
+        String error,
+        String message,
+        String path
+){}
